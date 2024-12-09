462eabbd2382cfa8f295a49cccc037ce5c892d8d
Juan Ernesto Rebatta Atoche <u202122626@upc.edu.pe>
2024-12-08 23:58:46 -0500

    cambiando de ambiente

--- scripts/ambiente.js
+++ scripts/ambiente.js
@@ -1,5 +1,5 @@
-//const API_BASE_URL = "https://be-bbtronic.onrender.com";
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://be-bbtronic.onrender.com";
+//const API_BASE_URL = "http://localhost:8080";
 
 
 export default API_BASE_URL;
