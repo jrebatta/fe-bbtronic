93ca129c5160054681e28aa7b5af76f1fbeaa126
juan-rebatta_pacifico <juan.rebatta@inetum.com>
2025-05-24 18:44:05 -0500

    front

--- scripts/ambiente.js
+++ scripts/ambiente.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "https://be-bbtronic.onrender.com";
 const API_BASE_URL = "https://be-bbtronic-production.up.railway.app";
-//const API_BASE_URL = "http://localhost:8080";
+//const API_BASE_URL = "http://localhost:8081";
 
 export default API_BASE_URL;
